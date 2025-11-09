92c81d33f301864f6a6f5c7b8fcada034bdd5f79
Pablo Gareis <gareispablotecnica1@gmail.com>
2025-11-09 02:03:52 -0300

    React

---
+++ Client/src/Components/Global/Header.jsx
@@ -0,0 +1,23 @@
+import { TfiCrown } from "react-icons/tfi";
+function Header() {
+    return (
+        <>
+            <header className="Encabezado">
+                <div className="header-logo">
+                    <div className="crown-icon">
+                        <TfiCrown />
+                    </div>
+                    <span className="logo-text">GAREIS S.A</span>
+                </div>
+
+                <nav className="Menu">
+                    <a href="#registrar">Registrar Usuario</a>
+                    <a href="#nosotros">Nosotros</a>
+                    <a href="#productos">Productos</a>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header
---
+++ Client/src/Components/Layouts.jsx
@@ -0,0 +1,16 @@
+import './Layouts.css';
+import Header from './Global/Header'
+import Footer from './Global/Footer'
+import RegistrarUsuario from './RegistrarUsuario';
+
+function Layouts() {
+    return (
+        <>
+            <Header />
+            <RegistrarUsuario />
+            <Footer />
+        </>
+    )
+}
+
+export default Layouts
